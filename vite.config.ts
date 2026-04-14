83d46789ccc4d9a23b44a8ff1e5824fa44e20a5d
Michal Talaga <mikeon@objectware.pl>
2026-04-14 10:55:57 +0200

    add vite tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -10,5 +11,9 @@ export default defineConfig({
   plugins: [tailwindcss(), react()],
   define: {
     __GIT_HASH__: JSON.stringify(gitHash),
+  },
+  test: {
+    include: ['src/**/*.{test,spec}.ts'],
+    environment: 'node',
   },
 })
